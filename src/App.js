42ccc42172f464191b7e78455302c9e494c796c7
Marcos Guibson <mgss.lic17@uea.edu.br>
2022-02-21 10:14:31 -0400

    feat: Finishing carousel project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
+import Logo from './assets/images/super-shoes.png';
+import Arrow from './assets/images/arrow.png';
 
 function App() {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(() =>{
+    fetch('http://localhost:3000/static/shoes.json')
+      .then((response) => response.json())
+      .then(setData)
+  }, [])
+
+  function handleLeftClick (e){
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  }
+    
+  
+
+  function handleRightClick (e){
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  }
+
+  if(!data || !data.length) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <img src={Logo} alt='Super-shoes Logo'/>
+      </div>
+      <div className="carousel" ref={carousel}>
+        {data.map((item) =>{
+          const {id, name, price, oldPrice, image} = item;
+          return(       
+            <div className='item' key={id}>
+              <div className='image'>
+                <img src={image} alt={name} />
+              </div>
+              <div className='info'>
+                <span className='name'>{name}1</span>
+                <span className='oldPrice'>{oldPrice}</span>
+                <span className='price'>{price}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className='buttons'>
+        <button onClick={handleLeftClick}>
+          <img src={Arrow} alt='Scroll Left'></img>
+        </button>
+        <button onClick={handleRightClick}>
+          <img src={Arrow} alt='Scroll Right'></img>
+        </button>
+      </div>
     </div>
   );
 }
